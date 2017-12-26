e4b2bb8e7347396c2845643435b50631de592da5
Travis CI User <travis@example.org>
2017-12-26 10:16:48 +0000

    Documentation for killercup/cargo-edit@330b012

---
+++ implementors/alloc/borrow/trait.ToOwned.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["ascii"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"ascii/struct.AsciiStr.html\" title=\"struct ascii::AsciiStr\">AsciiStr</a>",];
+implementors["openssl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"openssl/ssl/struct.SslSessionRef.html\" title=\"struct openssl::ssl::SslSessionRef\">SslSessionRef</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"openssl/x509/struct.X509Ref.html\" title=\"struct openssl::x509::X509Ref\">X509Ref</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["cargo_edit"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"cargo_edit/struct.Error.html\" title=\"struct cargo_edit::Error\">Error</a>",];
+implementors["cargo_metadata"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"cargo_metadata/struct.Error.html\" title=\"struct cargo_metadata::Error\">Error</a>",];
+implementors["error_chain"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
